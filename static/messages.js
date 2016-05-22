ddde7c5f6a5b9a91d1b8d93137b636f4bce5a365
Fiona Condon <frcondon@gmail.com>
2016-05-22 03:24:50 -0400

    Skip typing for longer strings, the dom can't handle it

--- static/messages.js
+++ static/messages.js
@@ -8,26 +8,34 @@ $( document ).ready(function() {
     }, 1000);
 
     var interval = 1;
+    var text_container = $("#text");
     // leaving a 30s grace period because the db can be laggy :(
     var time = (Date.now() / 1000) - (interval * 30);
     setInterval(function() {
         time += interval;
+        var url = "http://" + document.location.host + "/raw/" + time + "/" + (time + interval);
+
         $.ajax({
-            url: "http://" + document.location.host + "/raw/" + time + "/" + (time + interval)
+            url: url
         }).done(function(data) {
             if (data) {
-                typeOut(0, data + ". ");
+                data += ". ";
+                if (data.length < 100) {
+                    typeOut(0, data);
+                } else {
+                    // hundreds of element appends per second are apparently too much
+                    text_container.append(data);
+                }
             }
         });
     }, (interval * 1000));
 
-    var text_container = $("#text");
     function typeOut(i, text) {
         if (i < text.length) {
             text_container.append(text[i]);
 
             // random delay but make sure all text can be typed within the interval
-            var delay = (Math.random() * ((interval * 1000) / text.length));
+            var delay = (Math.random() * ((interval * 100) / text.length));
             setTimeout(function() {
                 typeOut(i + 1, text)
             }, delay);
