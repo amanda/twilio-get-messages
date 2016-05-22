c082a3ab2f7ce9f2fcc05caa3eb451d2c4314103
Fiona Condon <frcondon@gmail.com>
2016-05-22 00:53:46 -0400

    Design #2

--- static/messages.js
+++ static/messages.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
 
     setInterval(function() {
-        $('.prompt').addClass("dark");
+        $('#prompt').addClass("dark");
         setTimeout(function() {
-            $('.prompt').removeClass("dark");
+            $('#prompt').removeClass("dark");
         }, 500);
     }, 1000);
 
